71e5845e1fe443c5c2326fa48f60fbe1acae10ca
oxidetor <prateek.girdhar@outlook.com>
2022-07-09 19:59:07 -0700

    Add file structure and initial pseudocode

---
+++ lib/main.rb
@@ -0,0 +1,11 @@
+# Create a instance of the Game class
+# Select a random word from dictionary (5-12 chars)
+# Save selected word somewhere inside the game instance
+# Display an intro message to the user
+# Display the occluded word and number of guesses left to user
+# Prompt user to provide a letter
+# Check that letter is valid. If not display appropriate message to user
+# If letter is valid, check if it exists in saved word
+# Provide feedback for the user's guess
+# End the game if out of turns or saved word is guessed
+# Display final message to user based on result
